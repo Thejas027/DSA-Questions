01df0101007364b108333040390cc23decaa29ba
Thejas <teju270934@gmail.com>
2024-03-15 23:15:33 +0530

    Left rotation of an array by d places function added to program

--- Arrays/ArrOperations.cpp
+++ Arrays/ArrOperations.cpp
@@ -14,7 +14,7 @@ public:
         }
     }
 
-    // Left rotation of an array by one position
+    // Left rotation of an array by one position  BRUTE FROCE approach time complex = O(n)
     void leftRotate(int arr[], int n)
     {
         int temp = arr[0];
@@ -32,6 +32,57 @@ public:
         }
         cout << endl;
     }
+
+    // right rotaion of an array
+    void RightRotate(int arr[], int n)
+    {
+        int lastElement = arr[0];
+        for (int j = 0; j < n - 1; j++)
+        {
+            arr[j] = arr[j + 1];
+        }
+        arr[n - 1] = lastElement;
+        // to print the array
+        cout << "Array elements after rotation : ";
+        for (int k = 0; k < n; k++)
+        {
+            cout << arr[k] << " ";
+        }
+        cout << endl;
+    }
+
+    //  left rotaion of an array by 'd' places
+    void LeftRotation(int arr[], int d, int n)
+    {
+        d = d % n;
+        int *temp = new int[d];
+
+        // loop to add the d number of elements to temp
+        for (int i = 0; i < d; i++)
+        {
+            temp[i] = arr[i];
+        }
+
+        // loop to shift the elements
+        for (int j = d; j < n; j++)
+        {
+            arr[j - d] = arr[j];
+        }
+
+        // loop to add back the temp elements to main array
+        for (int i = (n - d); i < n; i++)
+        {
+            arr[i] = temp[i - (n - d)];
+        }
+
+        // loop to print the final array elements
+        cout << "Array elements after rotation : ";
+        for (int k = 0; k < n; k++)
+        {
+            cout << arr[k] << " ";
+        }
+        delete[] temp;
+    }
 };
 
 int main()
@@ -47,7 +98,8 @@ int main()
     {
         cout << "\nMENU\n";
         cout << "1. To read the array elements\n";
-        cout << "2. Left rotation of an array by one bit\n";
+        cout << "2. Rotations of an array by one bit\n";
+        cout << "3.To rotate the array by d place\n";
         cout << "Enter your choice : ";
         cin >> choice;
 
@@ -58,14 +110,22 @@ int main()
             break;
 
         case 2:
+            cout << "Right Rotation : ";
+            arr.RightRotate(a, n);
+            cout << "\nLeft Rotation : ";
             arr.leftRotate(a, n);
             break;
-
+        case 3:
+            int d;
+            cout << "Enter the d value to rotate the array : ";
+            cin >> d;
+            arr.LeftRotation(a, d, n);
+            break;
         default:
             cout << "Invalid choice\n";
             break;
         }
-    } while (choice != 3); // Adjusted loop condition to match menu options
+    } while (choice != 4); // Adjusted loop condition to match menu options
 
     return 0;
 }
