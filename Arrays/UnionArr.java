0f702b95c4bb6c00ec88cf4d9c7eaa1dd220453b
Thejas <teju270934@gmail.com>
2024-03-16 12:02:06 +0530

    program to union the given 2 array done in brute froce approcah

---
+++ Arrays/UnionArr.java
@@ -0,0 +1,43 @@
+package Arrays;
+
+import java.util.*;
+
+public class UnionArr {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the size of 1st Array :");
+        int n1 = sc.nextInt();
+        System.out.println("Enter the size of 2nd Array :");
+        int n2 = sc.nextInt();
+
+        int arr1[] = new int[n1];
+        int arr2[] = new int[n2];
+
+        // to give an input for 2 arrays
+        System.out.print("Enter the elements for 1st array : ");
+        for (int i = 0; i < n1; i++) {
+            arr1[i] = sc.nextInt();
+        }
+        System.out.print("Enter the elements for 2nd array : ");
+        for (int i = 0; i < n2; i++) {
+            arr2[i] = sc.nextInt();
+        }
+
+        // code to make the union of two arrays using set
+        Set<Integer> set = new HashSet<>();
+        for (int num : arr1) {
+            set.add(num);
+        }
+        for (int num : arr2) {
+            set.add(num);
+        }
+
+        System.out.print("Union of the two arrays: ");
+        for (int num : set) {
+            System.out.print(num + " ");
+        }
+        sc.close();
+    }
+
+}
