ef9586cb43ee8e4b4c7106a5867c3475cd8d87de
Thejas <teju270934@gmail.com>
2024-03-18 11:41:36 +0530

    program to print all the sub array of a given array added

---
+++ Arrays/SubArray/SubArrPrint.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    vector<int> arr = {1, 2, 3, 4, 5};
+    int n = arr.size();
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i; j < n; j++)
+        {
+
+            for (int k = i; k <= j; k++)
+            {
+                cout << arr[k] << " ";
+            }
+            cout << endl;
+        }
+        cout << endl;
+    }
+    return 0;
+}
