44001e962393fdb7d4b483a63fed7d25603474de
Thejas <teju270934@gmail.com>
2024-04-05 10:55:03 +0530

    program to return the count of a array element in a given sorted array using binary search

---
+++ BinarySearch/Bs-1/CountNum.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int CountNum(int *arr, int size, int key)
+{
+
+    int start = 0;
+    int end = size - 1;
+    int frst, last, result;
+
+    while (start <= end)
+    {
+        int mid = start + (end - start) / 2;
+
+        if (arr[mid] < key)
+            start = mid + 1;
+        else
+            end = mid - 1;
+    }
+    frst = start;
+
+    // reseting the start and end
+    start = 0;
+    end = size - 1;
+
+    while (start <= end)
+    {
+        int mid = start + (end - start) / 2;
+
+        if (arr[mid] > key)
+            end = mid - 1;
+        else
+            start = mid + 1;
+    }
+
+    last = end;
+
+    result = last - frst + 1;
+
+    return result;
+}
+
+int main()
+{
+    int arr[] = {1, 2, 2, 2, 3, 4, 5};
+    int target = 2;
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Count of target element: " << CountNum(arr, size, target) << endl;
+
+    return 0;
+}
