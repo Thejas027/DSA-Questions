5b4e5d6f9d211d12a698de979d15226c18c555df
Thejas <teju270934@gmail.com>
2024-05-23 00:49:27 +0530

    majority element, found with all the cases

--- Array_series/Arrays-2/video7.cpp
+++ Array_series/Arrays-2/video7.cpp
@@ -6,29 +6,79 @@ using namespace std;
 int majorityElement1(vector<int> arr)
 {
 
-      int ele = arr[0];
       int n = arr.size();
-      int count = 0;
 
       for (int i = 0; i < n; i++)
       {
-            if (ele == arr[i])
+            int count = 0;
+            for (int j = 0; j < n; j++)
             {
-                  count++;
-                  if (count > (n / 2))
-                        return arr[i];
+                  if (arr[i] == arr[j])
+                        count++;
             }
+            if (count > (n / 2))
+                  return arr[i];
       }
 
       return -1; // if majority element is not greater than the size of (N/2)
 }
 
+// better solution using maps
+int majorityElement2(vector<int> arr)
+{
+      int n = arr.size();
+
+      map<int, int> mp;
+      for (int i = 0; i < n; i++)
+            mp[arr[i]]++;
+
+      for (auto ele : mp)
+            if (ele.second > (n / 2))
+                  return ele.first;
+
+      return -1;
+}
+
+// optimal soultion
+
+int majorityElement3(vector<int> arr)
+{
+      int n = arr.size();
+      int ele = 0;
+      int count = 0;
+
+      for (int i = 0; i < n; i++)
+      {
+            if (count == 0)
+            {
+                  count++;
+                  ele = arr[i];
+            }
+
+            if (ele == arr[i])
+                  count++;
+            else
+                  count--;
+      }
+
+      int count1;
+      for (int i = 0; i < n; i++)
+      {
+            if (arr[i] == ele)
+                  count1++;
+
+            if (count1 > (n / 2))
+                  return ele;
+      }
+      return -1;
+}
+
 int main()
 {
 
-      vector<int> arr = {2, 2, 2, 3, 3, 4};
+      vector<int> arr = {2, 2, 2, 2, 4, 5, 3};
 
-      int element = majorityElement1(arr);
+      int element = majorityElement3(arr);
 
       if (element != -1)
             cout << "majority element : " << element << endl;
