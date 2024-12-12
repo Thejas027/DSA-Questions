636a0a77f7389849416370bdc312e4fb00f09f1a
Thejas <teju270934@gmail.com>
2024-12-12 23:52:55 +0530

    added 4 sum question problem

---
+++ Array_series/Array-1/4Sum/fourSum.cpp
@@ -0,0 +1,9 @@
+class solution
+{
+      vector<vector<int>> foursum(vector<int> &nums, int target)
+      {
+            int n = nums.size();
+            set<vector<int>> st;
+            
+      }
+}
--- Array_series/Array-1/NoOfSubArr/subarray.cpp
+++ Array_series/Array-1/NoOfSubArr/subarray.cpp
@@ -1,6 +1,6 @@
 // Problem to find number of subarrays in a given array with xor operations where it should be equal to k
 
-// brute force approach
+// brute force approach  --> tc = O(n^3)
 class solution
 {
       int countSubArray(vector<int> arr, int k)
@@ -13,6 +13,27 @@ class solution
                         int xor = 0;
                         for (int k = i; k < j; k++)
                               xor = xor ^ arr[k];
+                        if (xor== k)
+                              count++;
+                  }
+            }
+            return count;
+      }
+}
+
+// better with tc --> O(n^2)
+
+class solution
+{
+      int countSubArray(vector<int> arr, int k)
+      {
+            int count = 0;
+            for (int i = 0; i < arr.size(); i++)
+            {
+                  int xor = 0;
+                  for (int j = i; j < arr.size(); j++)
+                  {
+                        xor = xor ^ arr[j];
                         if (xor== k)
                               count++;
                   }
