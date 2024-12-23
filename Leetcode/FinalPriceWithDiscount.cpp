9960259fce034648984a5edaeefb4d4dd699ff0e
Thejas <teju270934@gmail.com>
2024-12-23 23:56:49 +0530

    added another program with some changes

--- Leetcode/FinalPriceWithDiscount.cpp
+++ Leetcode/FinalPriceWithDiscount.cpp
@@ -1,20 +1,21 @@
-class Solution {
+class Solution
+{
 public:
-    vector<int> finalPrices(vector<int>& prices) {
+    vector<int> finalPrices(vector<int> &prices)
+    {
         int n = prices.size();
-        vector<int> answer(n); // To store the final prices
-        stack<int> st; // Monotonic stack to keep track of indices
+        vector<int> answer(n);
+        stack<int> st;
 
-        for (int i = 0; i < n; i++) {
-            // Check for discounts for earlier items
-            while (!st.empty() && prices[st.top()] >= prices[i]) {
+        for (int i = 0; i < n; i++)
+        {
+            while (!st.empty() && prices[st.top()] >= prices[i])
+            {
                 int index = st.top();
                 st.pop();
                 answer[index] = prices[index] - prices[i];
             }
-            // Push the current index onto the stack
             st.push(i);
-            // Temporarily store the original price in the answer
             answer[i] = prices[i];
         }
 
