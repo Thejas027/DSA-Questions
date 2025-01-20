b74ffcb04175cf8948b8942123a75229664c92d5
Thejas <teju270934@gmail.com>
2025-01-20 23:03:54 +0530

    updated the code

--- Array_Series/spiralMatrix.cpp
+++ Array_Series/spiralMatrix.cpp
@@ -8,6 +8,7 @@ public:
             int startRow = 0;
             int endRow = matrix[0].size();
             vector<int> ans;
+
             while (startCol <= endCol && startRow <= endRow)
             {
                   // print the top
@@ -17,13 +18,15 @@ public:
                   // print the right
                   for (int i = startRow + 1; i <= endRow; i++)
                         ans.push_back(matrix[i][endCol]);
-
+                        
+                  // print the bottom
                   if (startRow < endRow)
                   {
                         for (int i = endCol - 1; i >= startCol; i--)
                               ans.push_back(matrix[endRow][i]);
                   }
 
+                  // print the left
                   if (startCol < endCol)
                   {
                         for (int i = endRow - 1; i > startRow; i--)
@@ -37,5 +40,3 @@ public:
             return ans;
       }
 }
-
-
