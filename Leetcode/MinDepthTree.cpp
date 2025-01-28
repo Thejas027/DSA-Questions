d47b04d29c7c95d3b00a1ada99edb72675ee35cb
Thejas <teju270934@gmail.com>
2025-01-28 18:46:41 +0530

    added leetcode question number and description

--- Leetcode/MinDepthTree.cpp
+++ Leetcode/MinDepthTree.cpp
@@ -1,3 +1,14 @@
+/*
+LEET CODE : 111 Minimum depth of a give binary tree
+
+Given a binary tree, find its minimum depth.
+
+The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
+
+Note: A leaf is a node with no children.
+
+*/
+
 class solution
 {
 public:
