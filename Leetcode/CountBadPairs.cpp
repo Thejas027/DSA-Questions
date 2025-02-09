06ee98e1da5a551b88e92d6efe690e61acab0519
Thejas <teju270934@gmail.com>
2025-02-09 18:35:30 +0530

    added the optimal approoch to find the badpairs count

--- Leetcode/CountBadPairs.cpp
+++ Leetcode/CountBadPairs.cpp
@@ -1,5 +1,5 @@
-//Leet code : 2364 (Feb 9th daily question)
-// Brute force approach to count the bad paris in the given array 
+// Leet code : 2364 (Feb 9th daily question)
+//  Brute force approach to count the bad paris in the given array
 class solution
 {
 public:
@@ -15,6 +15,29 @@ public:
                         if (nums[j] - j != nums[i] - i)
                               count++;
                   }
+            }
+            return count;
+      }
+}
+
+// optimal way using hasp map
+
+class solution
+{
+public:
+      long long countBadParis(vector<int> &nums)
+      {
+            long long count = 0;
+            unordered_map<int, int> mapCount;
+
+            for (int i = 0; i < n; i++)
+            {
+                  int diff = i - nums[i];
+
+                  int goodParis = mapCount[diff];
+                  count += i - goodParis;
+
+                  mapCount[diff] = goodParis + 1;
             }
             return count;
       }
