4c36f60c7b2628d79bd0f3d51260e584a0211c2f
Thejas <teju270934@gmail.com>
2025-02-26 22:41:24 +0530

    maximum abs sum of an subarry using prefix sum  method

---
+++ Leetcode/MaxAbsSubArrSum.cpp
@@ -0,0 +1,18 @@
+// Leet code : 1749. Maximum Absolute Sum of Any Subarray
+
+class solution
+{
+      int maxSubArrSum(vector<int> &nums)
+      {
+            int minPrefixSum = 0, maxPrefixSum = 0;
+            int prefixSum = 0;
+
+            for (int i = 0; i < nums.size(); i++)
+            {
+                  prefixSum += nums[i];
+                  minPrefixSum = min(minPrefixSum, prefixSum);
+                  maxPrefixSum = max(maxPrefixSum, prefixSum);
+            }
+            return maxPrefixSum - minPrefixSum;
+      }
+}
