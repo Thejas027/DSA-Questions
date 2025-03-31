93e108d21558a9ec42e09a650fe377e88735d46f
Thejas027 <teju270934@gmail.com>
2025-03-31 15:54:03 +0530

    added the space optimized code for frog jump with one or two steps

--- DynamicProgramming/FrogJump.cpp
+++ DynamicProgramming/FrogJump.cpp
@@ -1,3 +1,4 @@
+// Tabulation method
 class solution
 {
 public:
@@ -12,5 +13,27 @@ public:
                   dp[i] = max(max(stones[i] - stones[i - 1], dp[i - 1]), max(stones[i] - stones[i - 2], dp[i - 2]));
 
             return dp[n - 1];
+      }
+}
+
+// Space optimized
+class sol
+{
+public:
+      int maxJump(vector<int> &stones)
+      {
+            int prev1 = 0, prev2 = 0;
+            for (int i = 1; i < stones.size(); i++)
+            {
+                  int left = prev + abs(stones[i] - stones[i - 1]);
+                  int right = __INT_MAX__;
+                  if (i > 1)
+                        right = prev2 + abs(stones[i] - stones[i - 2]);
+
+                  int curr = min(left, right);
+                  prev2 = prev1;
+                  prev1 = curr;
+            }
+            return prev1;
       }
 }
