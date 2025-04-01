afd0c7e7d4ae000225127277898c6b978276b85a
Thejas027 <teju270934@gmail.com>
2025-04-01 19:35:59 +0530

    sloved todays daily challange question

---
+++ DynamicProgramming/BrainPowerQues.cpp
@@ -0,0 +1,27 @@
+/*You are given a 0-indexed 2D integer array questions where questions[i] = [points(i), brainpower(i)].
+
+The array describes the questions of an exam, where you have to process the questions in order (i.e., starting from question 0) and make a decision whether to solve or skip each question. Solving question i will earn you points(i) points but you will be unable to solve each of the next brainpower(i) questions. If you skip question i, you get to make the decision on the next question.
+
+For example, given questions = [[3, 2], [4, 3], [4, 4], [2, 5]]:
+If question 0 is solved, you will earn 3 points but you will be unable to solve questions 1 and 2.
+If instead, question 0 is skipped and question 1 is solved, you will earn 4 points but you will be unable to solve questions 2 and 3.
+Return the maximum points you can earn for the exam.*/
+// Bottom up approach
+class solution
+{
+      long long mostPoints(vector<vector<int>> &questions)
+      {
+            int n = questions.size();
+            vector<long long> dp(n + 1, 0);
+
+            for (int i = n - 1; i >= 0; --i)
+            {
+                  int points = questions[i][0];
+                  int brainPower = questions[i][1];
+                  int nextQuestion = i + brainPower + 1;
+
+                  dp[i] = max(points + (nextQuestion < n ? dp[nextQuestion] : 0), dp[i - 1]);
+            }
+            return dp[0];
+      }
+}
