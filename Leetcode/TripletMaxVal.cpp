574e25fdc3fc2e550bc4b2392c9fd2bfb1c993f4
Thejas027 <teju270934@gmail.com>
2025-04-03 17:41:44 +0530

    sloved another question on triplet sum

--- Leetcode/TripletMaxVal.cpp
+++ Leetcode/TripletMaxVal.cpp
@@ -51,11 +51,11 @@ public:
 
             leftMin[0] = nums[0];
             for (int i = 1; i < n; i++)
-                  leftMin[i] = min(leftMin[i], nums[i]);
+                  leftMin[i] = min(leftMin[i - 1], nums[i]);
 
             rightMin[n - 1] = nums[n - 1];
             for (int i = n - 2; i >= 0; i--)
-                  rightMin[i] = min(rightMin[i], nums[i]);
+                  rightMin[i] = min(rightMin[i + 1], nums[i]);
 
             int res = INT_MAX;
             for (int i = 1; i < n - 1; i++)
@@ -63,6 +63,35 @@ public:
                   if (leftMin[i - 1] < nums[i] && rightMin[i + 1] < nums[i])
                         res = min(res, leftMin[i - 1] + nums[i] + rightMin[i + 1]);
             }
-            return (res == INT_MAX) ? -1 : res;   
+            return (res == INT_MAX) ? -1 : res;
+      }
+}
+
+// Sum of beauties
+class solution
+{
+public:
+      int sumBeauty(vector<int> &nums)
+      {
+            int n = nums.size();
+            int sum = 0;
+            vector<int> leftMin(n, INT_MAX), rightMin(n, INT_MAX);
+
+            leftMin[0] = nums[0];
+            for (int i = 1; i < n; i++)
+                  leftMin[i] = max(leftMin[i - 1], nums[i]);
+
+            rightMin[n - 1] = nums[n - 1];
+            for (int i = n - 2; i >= 0; i--)
+                  rightMin[i] = min(rightMin[i + 1], nums[i]);
+
+            for (int i = 1; i < n - 1; i++)
+            {
+                  if (leftMin[i - 1] < nums[i] && nums[i] < rightMin[i + 1])
+                        sum += 2;
+                  else if (nums[i - 1] < nums[i] && nums[i] < nums[i + 1])
+                        sum += 1;
+            }
+            return sum;
       }
 }
