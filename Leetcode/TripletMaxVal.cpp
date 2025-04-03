af1271429c7c6e997a032c97b4afa93c1975db49
Thejas027 <teju270934@gmail.com>
2025-04-03 17:15:56 +0530

    similar question regarding triplet has been sloved

--- Leetcode/TripletMaxVal.cpp
+++ Leetcode/TripletMaxVal.cpp
@@ -36,3 +36,33 @@ public:
             return ans;
       }
 }
+
+// Minimum sum of mountain triplets
+class solution
+{
+public:
+      int minMountain(vector<int> &nums)
+      {
+            int n = nums.size();
+            if (n < 3)
+                  return -1;
+
+            vector<int> leftMin(n, INT_MAX), rightMin(n, INT_MAX);
+
+            leftMin[0] = nums[0];
+            for (int i = 1; i < n; i++)
+                  leftMin[i] = min(leftMin[i], nums[i]);
+
+            rightMin[n - 1] = nums[n - 1];
+            for (int i = n - 2; i >= 0; i--)
+                  rightMin[i] = min(rightMin[i], nums[i]);
+
+            int res = INT_MAX;
+            for (int i = 1; i < n - 1; i++)
+            {
+                  if (leftMin[i - 1] < nums[i] && rightMin[i + 1] < nums[i])
+                        res = min(res, leftMin[i - 1] + nums[i] + rightMin[i + 1]);
+            }
+            return (res == INT_MAX) ? -1 : res;   
+      }
+}
